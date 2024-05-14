938d0ab7f194290c38edb4f261d8ce5b6dd5ef09
dgarx <dgarx@Kali.david.org>
2024-05-14 19:00:05 +0200

    Added some files

---
+++ NET/CYBERMAN/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace pito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string readText = File.ReadAllText(@"../../../Greetings/1.txt");
+            Console.WriteLine(readText);
+        }
+    }
+}
